69cc1a7e57564a23a48d7adb7d4b650b742a63c2
sum <104844080+yunyzzang@users.noreply.github.com>
2023-06-16 18:03:33 +0200

    Add files via upload

---
+++ 1_libft/ft_strjoin.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sum <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 17:17:37 by sum               #+#    #+#             */
+/*   Updated: 2022/10/04 17:23:15 by sum              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*new;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	new = (char *)ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), 1);
+	if (!new)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+		new[i++] = s2[j++];
+	new[i] = '\0';
+	return (new);
+}
+
+/*int	main()
+{
+	const char *s1 = "lorem ipsum";
+	const char *s2 = "dolor sit amet";
+	
+	printf("%s", ft_strjoin(s1,s2));
+}*/
---
+++ 1_libft/ft_strlen.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sum <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 14:30:59 by sum               #+#    #+#             */
+/*   Updated: 2022/10/03 14:31:09 by sum              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
+/*int	main(void)
+{
+	char *str, *str2;
+
+	str = "Hello ";
+	str2 = "Pineapple";
+	printf("expected: %lu\n", strlen(str));
+	printf("yours: %d\n\n", ft_strlen(str));
+        printf("expected: %lu\n", strlen(str2));
+        printf("yours: %d\n", ft_strlen(str2));
+}*/
